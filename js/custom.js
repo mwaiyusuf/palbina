0e529211b6c344da5599c9438fe4c026c552c6b6
mohammed yusuf <mwaigalo5@gmail.com>
2018-04-24 11:15:31 +0300

    p

---
+++ js/custom.js
@@ -0,0 +1,7 @@
+var x=prompt("Enter your name");
+document.write("hallo " + x);
+var y=prompt("Enter your digits");
+parseInt(y);
+document.write( y + " this is your code ");
+var z=prompt(" whats your favourite color");
+Document.write( z + " is a color for silly people ");

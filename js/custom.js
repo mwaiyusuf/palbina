de88cd2b0c92f9e3acf675f188f8d437dbd596a7
mohammed yusuf <mwaigalo5@gmail.com>
2018-05-08 11:47:31 +0300

    e

--- js/custom.js
+++ js/custom.js
@@ -3,5 +3,5 @@ document.write("hallo " + x);
 var y=prompt("Enter your digits");
 parseInt(y);
 document.write( y + " this is your code ");
-var z=prompt(" which is your ");
+var z=prompt(" which is your favourite");
 Document.write( z + " is a color for silly people ");

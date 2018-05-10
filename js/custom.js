13e8e51977619257b44e415793dfb4152602f4be
mohammed yusuf <mwaigalo5@gmail.com>
2018-05-10 23:50:07 +0300

    w

--- js/custom.js
+++ js/custom.js
@@ -3,5 +3,5 @@ document.write("hallo " + x);
 var y=prompt("Enter three  digits from 0-9 ");
 parseInt(y);
 document.write( y + " this is your code ");
-var z=prompt(" which is your favourite country on");
+var z=prompt(" which is your favourite country on this");
 Document.write( z + " is an amazing country,having awesome sites ");

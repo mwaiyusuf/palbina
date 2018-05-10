cd0abdbbdf712a8dc6b70794a9f53e76b6d8716c
mohammed yusuf <mwaigalo5@gmail.com>
2018-05-10 23:48:15 +0300

    r

--- js/custom.js
+++ js/custom.js
@@ -1,7 +1,7 @@
 var x=prompt("Enter your name");
 document.write("hallo " + x);
-var y=prompt("Enter three  digits from 0-9");
+var y=prompt("Enter three  digits from 0-9 ");
 parseInt(y);
 document.write( y + " this is your code ");
-var z=prompt(" which is your favourite country");
+var z=prompt(" which is your favourite country on");
 Document.write( z + " is an amazing country,having awesome sites ");
